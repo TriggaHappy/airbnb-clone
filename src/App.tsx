286acea369dc175128bd28edab58f2412f41d232
Maximilian Merten <maximilian.merten@ewerk.com>
2024-09-27 11:30:46 +0200

    adds navbar and hero component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import Navbar from "./components/navbar";
+import Hero from "./components/hero.tsx";
 
 export default function App() {
   return (
     <>
-      <Navbar />
       <main className="min-h-screen">
-        <div className="flex bg-white text-9xl font-bold text-red-900"></div>
+          <Navbar />
+            <Hero/>
       </main>
     </>
   );
