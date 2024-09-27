266c09129b4d6285af5483f0dcfcd751eb1b005b
Maximilian Merten <maximilian.merten@ewerk.com>
2024-09-27 11:30:59 +0200

    adds hero component

---
+++ src/components/hero.tsx
@@ -0,0 +1,18 @@
+import HeroImage from "../assets/img.png";
+
+export default function Hero() {
+  return (
+    <>
+      <div className="flex-box mt-8 pl-20">
+        <img src={HeroImage} />
+      </div>
+      <div className="p-10">
+        <h1 className="text-5xl font-extrabold"> Online Experience</h1>
+        <div className="w-1/3 pt-5">
+          Join unique interactive activities led by one-of-a-kind hosts-all
+          without leaving home.
+        </div>
+      </div>
+    </>
+  );
+}
